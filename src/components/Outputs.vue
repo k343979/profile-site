b7bb310764402d9464246a370c27c2d579f7b54a
k343979 <yusuke040989@gmail.com>
2020-09-18 20:35:53 +0900

    ファイル修正

--- src/components/Outputs.vue
+++ src/components/Outputs.vue
@@ -5,15 +5,19 @@
       <div class="portfolio-finished">
         <h2>♦︎ポートフォリオ完成</h2>
         <div class="main-content">
-          <h3>{{ finished_name }}</h3>
-          <p>{{ finished_text }}</p>
+          <ul v-for="(item, index) in finishLists" :key="index">
+            <li><h3>{{ item.finished_name }}</h3></li>
+            <li><p>{{ item.finished_text }}</p></li>
+          </ul>
         </div>
       </div>
       <div class="portfolio-making">
         <h2>♦︎ポートフォリオ制作中</h2>
         <div class="main-content">
-          <h3>{{ making_name }}</h3>
-          <p>{{ making_text }}</p>
+          <ul v-for="(item, index) in makingLists" :key="index">
+            <li><h3>{{ making_name }}</h3></li>
+            <li><p>{{ making_text }}</p></li>
+          </ul>
         </div>
       </div>
     </div>
@@ -27,10 +31,22 @@ export default {
   data() {
     return {
       page_name: "過去の制作物/ポートフォリオ",
-      finished_name: "■ 卒業論文：カジノ施設の設立による経済への影響",
-      finished_text: "日本にカジノが導入される事によって、期待できる「経済効果」と起こりうる「社会的影響」について研究しました。",
-      making_name: "■ telemanage",
-      making_text: "テレワーク制度下において業務管理ができるWebアプリケーションを開発しました。2020年より新型コロナウイルスの影響を受け、テレワーク制度の導入が推奨されています。パンデミックを発端に、今後リモートワークという働き方が当たり前になる時代が到来してもおかしくありません。しかしそのような社会情勢下においても、未だにテレワークへ移行できていない企業様もいらっしゃいます。おそらくその原因は、「部下が出社せずにきちんと仕事に取り組めているのか、監視できない。」、「業務において不明点があった時、すぐに上司に質問できない。」などといったことだと考えました。これらを払拭できるような便利なツールがあれば、リモートワークを促進できるのではないかと考え、「telemanage」というWebアプリケーションを開発しました。離れていても業務の進捗状況を可視的に把握できる機能や、すぐに質問できるチャット機能を搭載しています。"
+      finishLists: [
+        {
+          finished_name: "■ 卒業論文：カジノ施設の設立による経済への影響",
+          finished_text: "日本にカジノが導入される事によって、期待できる「経済効果」と起こりうる「社会的影響」について研究しました。"
+        },
+        {
+          finished_name: "■ telemanage",
+          finished_text: "テレワーク制度下において業務管理ができるWebアプリケーションを開発しました。2020年より新型コロナウイルスの影響を受け、テレワーク制度の導入が推奨されています。パンデミックを発端に、今後リモートワークという働き方が当たり前になる時代が到来してもおかしくありません。しかしそのような社会情勢下においても、未だにテレワークへ移行できていない企業様もいらっしゃいます。おそらくその原因は、「部下が出社せずにきちんと仕事に取り組めているのか、監視できない。」、「業務において不明点があった時、すぐに上司に質問できない。」などといったことだと考えました。これらを払拭できるような便利なツールがあれば、リモートワークを促進できるのではないかと考え、「telemanage」というWebアプリケーションを開発しました。離れていても業務の進捗状況を可視的に把握できる機能や、すぐに質問できるチャット機能を搭載しています。"
+        }
+      ],
+      makingLists: [
+        {
+          making_name: "",
+          making_text: ""
+        }
+      ]
     };
   },
   components: {
@@ -48,6 +64,10 @@ export default {
 .main {
   padding: 0 50px;
   margin-bottom: 40px;
+}
+
+ul {
+  list-style: none;
 }
 
 h2 {
--- src/components/Tabmenu.vue
+++ src/components/Tabmenu.vue
@@ -9,25 +9,6 @@
         </li>
       </ol>
     </div>
-    <div class="hamburger">
-      <div class="hamburger_btn" @click="openMenu">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <transition name="menu">
-        <div class="manu" v-if="show">
-          <ul>
-            <li v-for="(item, index) in items" :key="index">
-              <router-link v-bind:to="item.path">
-                {{ item.title }}
-              </router-link>
-            </li>
-            <button @click="closeManu">閉じる</button>
-          </ul>
-        </div>
-      </transition>
-    </div>
   </div>
 </template>
 
@@ -43,17 +24,8 @@ export default {
         { title: 'skills', path: '/skills' },
         { title: 'outputs', path: '/outputs' }
       ],
-      show: false
     };
   },
-  methods: {
-    openMenu() {
-      this.show = true;
-    },
-    closeMenu() {
-      this.show = false;
-    }
-  }
 };
 </script>
 
@@ -97,44 +69,5 @@ a:hover {
   border: 1px solid #444;
   border-radius: 8px;
   color: #444;
-}
-
-@media screen and (max-width: 768px) {
-  #hamburger {
-    width: 40px;
-    height: 40px;
-    position: absolute;
-    cursor: pointer;
-    
-    top: 15px;
-    right: 20px;
-    transition: 0.4s;
-  }
-
-  #hamburger span {
-    position: absolute;
-    left: 2px;
-    display: block;
-    width: 35px;
-    height: 2px;
-    color: #444;
-    transition: 0.8s;
-  }
-
-  #hamburger span:nth-of-type(1) {
-    top: 10px;
-  }
-
-  #hamburger span:nth-of-type(2) {
-    top: 20px;
-  }
-
-  #hamburger span:nth-of-type(3) {
-    top: 30px;
-  }
-
-  .open {
-    display: none;
-  }
 }
 </style>
